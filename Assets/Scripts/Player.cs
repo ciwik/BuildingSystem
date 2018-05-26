31cbf66d7c7f28ba8193de790b91f74befddc1f0
ciwik <lgnv2009@gmail.com>
2018-05-26 19:26:05 +0400

    Enable gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 0.25f;
+    private float _speed = 10f;
 
     private CharacterController _characterController;
     private IInputController _inputController;
@@ -25,6 +25,6 @@ public class Player : MonoBehaviour
     private void Move(Vector3 direction, Quaternion rotation)
     {
         transform.rotation *= rotation;
-        _characterController.Move(transform.rotation * direction * _speed);        
+        _characterController.SimpleMove(transform.rotation * direction * _speed);        
     }
 }
